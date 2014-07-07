f9806eb6030c2304aedbd33be2b636feb29b09b2
Jovi Kao <jovikao@invisibi.com>
2014-07-07 14:13:05 +0800

    not extend recurlyobject

--- src/main/java/com/ning/billing/recurly/model/DiscountInCents.java
+++ src/main/java/com/ning/billing/recurly/model/DiscountInCents.java
@@ -1,7 +1,5 @@
 package com.ning.billing.recurly.model;
 
-import com.ning.billing.recurly.model.RecurlyObject;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -9,7 +7,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Class that represents the Concept of a Coupon within the Recurly API.
  */
 @XmlRootElement(name = "discount_in_cents")
-public class DiscountInCents extends RecurlyObject {
+public class DiscountInCents {
 
 
     @XmlElement(name = "USD")
